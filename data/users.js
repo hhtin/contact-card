c19201933e2849410c35dea9c680db90e12c83a9
tinhh <tinhhdev@gmail.com>
2025-05-14 14:17:48 +0700

    ++ commit

--- data/users.js
+++ data/users.js
@@ -2,17 +2,16 @@ export const users = {
   almira: {
     name: "Nguyễn Hoàng Ánh Nhi",
     description: "Chuyên viên đối ngoại tại JobsGO!",
-    avatar: "https://i.imgur.com/yjjPIGc.jpeg",
+    avatar: "https://i.imgur.com/PtN1C2h.jpeg",
     socials: [
       { name: "Facebook", text: "Nguyễn Hoàng Ánh Nhi", href: "https://www.facebook.com/anhnhinguyen23", icon: "fab fa-facebook-f", gradient: "from-[#1877F2] to-blue-400" },
       { name: "Instagram", text: "ann_hhy", href: "https://www.instagram.com/ann_hhy/", icon: "fab fa-instagram", gradient: "from-purple-600 via-pink-500 to-orange-400" },
       { name: "TikTok", text: "almira_thienbinht10", href: "https://www.tiktok.com/@almira_thienbinht10", icon: "fab fa-tiktok", gradient: "from-[#EE1D52] via-[#000000] to-[#69C9D0]" },
       { name: "Zalo", text: "Zalo", href: "https://zalo.me/0345505870", icon: "fas fa-comment-alt", gradient: "from-[#0068FF] to-blue-300" },
-      { name: "Private Phone", text: "0345505870", href: "tel:0345505870", icon: "fas fa-phone", gradient: "from-gray-700 to-gray-400" },
-      { name: "Momo", text: "0345505870", href: "https://nhantien.momo.vn/0345505870", icon: "fas fa-wallet", gradient: "from-[#A50064] to-[#D82D8B]",isMomo:true },
-      { name: "Timo", text: "0345505870", href: "#", icon: "fas fa-university", gradient: "from-[#9A1AFF] to-purple-400", copyable: true },
+      { name: "Phone", text: "0767231022", href: "tel:0767231022", icon: "fas fa-phone", gradient: "from-gray-700 to-gray-400" },
+      { name: "Momo", text: "Momo", href: "https://nhantien.momo.vn/0345505870", icon: "fas fa-wallet", gradient: "from-[#A50064] to-[#D82D8B]",isMomo:true },
+      { name: "Timo", text: "9021982289073", href: "#", icon: "fas fa-university", gradient: "from-[#9A1AFF] to-purple-400", copyable: true },
       { name: "Vietcombank", text: "1029127347", href: "#", icon: "fas fa-university", gradient: "from-[#018143] to-green-300", copyable: true },
-      
     ],
   },
   anotheruser: {
